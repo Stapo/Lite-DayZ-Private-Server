a030b28c093565b47b3c5fac26d542ec4b5950a3
Lional Stapleton <stapo19@hotmail.com>
2012-12-13 16:53:10 -0800

    Added MySQL Database
    Port 3306

--- Start Server.bat
+++ Start Server.bat
@@ -1,5 +1,5 @@
 @echo off
 start /D.\MySQL\ MySQL.bat
-.\MySQL\bin\mysql --user=root --password=root --host=127.0.0.1 --port=3316 --database=hivemind --execute="call pMain()"
+.\MySQL\bin\mysql --user=root --password=root --host=127.0.0.1 --port=3306 --database=hivemind --execute="call pMain()"
 start .\Expansion\beta\arma2oaserver.exe -port=2302 -mod=@dayz;@hive -name=DayZ -config=DayZConfig\ServerSettings.cfg -cfg=DayZConfig\Arma2Config.cfg -profiles=DayZConfig
 exit
